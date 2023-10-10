bc8f7e47c5bb646587ba3262e8338a499b110713
Israel Rocha <israel.rexc@yahoo.com>
2023-10-10 02:58:37 -0300

    feat: Implementa scrap

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -4,6 +4,8 @@ namespace Chuva\Php\WebScrapping;
 
 use Chuva\Php\WebScrapping\Entity\Paper;
 use Chuva\Php\WebScrapping\Entity\Person;
+use DOMXPath;
+use LengthException;
 
 /**
  * Does the scrapping of a webpage.
@@ -14,8 +16,55 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-    
-    return [];
+    $xpath = new \DOMXPath($dom);
+    $cards = $xpath->query("//*[@class='paper-card p-lg bd-gradient-left']");
+    $papers = [];
+
+    if ($cards === NULL) {
+      return [];
+    }
+
+    foreach ($cards as $card) {
+      $persons = [];
+
+      if (($card->childNodes === NULL) || ($card->childNodes->length !== 3)) {
+        throw new \LengthException("Each card must have 3 children");
+      }
+
+      // Nodes
+      $title = $card->firstChild;
+      $authorsGroup = $title->nextSibling;
+      $footer = $authorsGroup->nextSibling; // Get card footer
+
+      $type = $footer->firstChild;
+      $id = ($type->nextSibling)->lastChild;
+
+      // Node list
+      $authors = $authorsGroup->childNodes;
+
+      // Extract author's name and institution
+      foreach ($authors as $author) {
+
+        // Skips whitespace
+        if (ctype_space($author->nodeValue)) {
+          continue;
+        }
+
+        $institutions = $author->attributes->getNamedItem('title');
+
+        $persons[] = new Person($author->nodeValue, $institutions->nodeValue);
+      }
+
+      // Builds object
+      $papers[] = new Paper(
+        $id->nodeValue,
+        $title->nodeValue,
+        $type->nodeValue,
+        $persons
+      );
+    }
+    return $papers;
+
     // return [
     //   new Paper(
     //     123,
