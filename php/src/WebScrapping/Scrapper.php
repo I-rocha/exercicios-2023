9c6acd68b43f7da7bb302819c741a346ad632ff4
Israel Rocha <israel.rexc@yahoo.com>
2023-10-10 02:10:09 -0300

    Comenta exemplo de retorno do scrap

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -14,17 +14,19 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
+    
+    return [];
+    // return [
+    //   new Paper(
+    //     123,
+    //     'The Nobel Prize in Physiology or Medicine 2023',
+    //     'Nobel Prize',
+    //     [
+    //       new Person('Katalin Karikó', 'Szeged University'),
+    //       new Person('Drew Weissman', 'University of Pennsylvania'),
+    //     ]
+    //   ),
+    // ];
   }
 
 }
